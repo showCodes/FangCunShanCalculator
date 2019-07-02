9387afc1c4e2da9666fd3512d3330998456cc6ff
showCodes <18390227669@163.com>
2019-07-02 21:36:51 +0800

    创建客户端

---
+++ src/Client.java
@@ -0,0 +1,10 @@
+import javax.script.ScriptException;
+
+public class Client {
+
+    public static void main(String[] args) throws ScriptException {
+
+        CalcData calcData = new CalcData();
+        calcData.printResults();
+    }
+}

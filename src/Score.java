d7c235a2cdd67e360cb5cbabd0099e3ef5387e13
showCodes <18390227669@163.com>
2019-07-02 20:17:51 +0800

    创建每个位置上的得分类

---
+++ src/Score.java
@@ -0,0 +1,34 @@
+public class Score {
+
+    private double maxScore;
+    private double curScore;
+    private double minScore;
+
+    public Score() {
+    }
+
+    public double getMaxScore() {
+        return maxScore;
+    }
+
+    public void setMaxScore(double maxScore) {
+        this.maxScore = maxScore;
+    }
+
+    public double getCurScore() {
+        return curScore;
+    }
+
+    public void setCurScore(double curScore) {
+        this.curScore = curScore;
+    }
+
+    public double getMinScore() {
+        return minScore;
+    }
+
+    public void setMinScore(double minScore) {
+        this.minScore = minScore;
+    }
+}
+

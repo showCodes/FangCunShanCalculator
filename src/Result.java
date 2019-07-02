b74b7773194112d77c7f05b769497ff2ac9b36ab
showCodes <18390227669@163.com>
2019-07-02 20:20:08 +0800

    创建结果类

---
+++ src/Result.java
@@ -0,0 +1,25 @@
+public class Result {
+
+    private String equation;
+    private int res;
+
+    public Result(String equation) {
+        this.equation = equation;
+    }
+
+    public String getEquation() {
+        return equation;
+    }
+
+    public void setEquation(String equation) {
+        this.equation = equation;
+    }
+
+    public int getRes() {
+        return res;
+    }
+
+    public void setRes(int res) {
+        this.res = res;
+    }
+}
